b9dd28f20a4c3c180afae985b0aa02a2292ab10b
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-08-14 12:53:42 -0700

    Agregar archivos de proyecto.

---
+++ CargarImagen/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Emgu;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+
+namespace CargarImagen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string imagen = "C:\\Users\\alumno\\Desktop\\Sora.jpg";
+            Image<Bgr, byte> img = new Image<Bgr, byte>(imagen);
+
+            CvInvoke.Imshow("imagen",img);
+            CvInvoke.WaitKey(0);
+        }
+    }
+}
